47eb29b42fc9c959f6f2f1ccc8cdd64d5efcec66
Helen Ngo <112019226+helenngo733@users.noreply.github.com>
2024-12-11 13:02:57 -0800

    added

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cmd_shell.h"
+
+int main() {
+    char input[MAX_INPUT_LEN];
+
+    while (1) {
+        printf("xsh# ");
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            perror("failed to read input");
+            break;
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+        if (strcmp(input, "quit") == 0 || strcmp(input, "exit") == 0)
+            break;
+
+        if (strchr(input, '|')) {
+            piping(input);
+        } else {
+            replace_env_vars(input);
+            commands(input);
+        }
+    }
+    return 0;
+}
